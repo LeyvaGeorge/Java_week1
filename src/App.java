64199df4d4557c73f281c2ce04da186e3110e5e3
George Leyva <leyvageorge@hotmail.com>
2026-01-16 13:30:08 -0800

    Finished Project

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
+    // App prints basic information
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Prints hard coded message
+        System.out.println("First Name: James");
+        System.out.println("Last Name: Smith");
+        System.out.println("Street Address: 123 California Dr.");
+        System.out.println("City: Los Angeles");
+        System.out.println("Zip code: 90012");
     }
 }
